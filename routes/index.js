32acc22939e8c98c17cc45791d7ba10d0cbf60c3
congtruqn <congtruqn@gmail.com>
2022-04-15 23:49:49 +0700

    1

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var cors = require('cors')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let url = process.env.REV
+  let url = process.env.HOST
   res.json({
     data:url
   });
