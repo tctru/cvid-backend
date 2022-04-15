34c899ce5bc08fa48f37d085b6f8b0827bfaf1f2
congtruqn <congtruqn@gmail.com>
2022-04-15 22:41:08 +0700

    1

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var cors = require('cors')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let url = process.env._MONGO_DSN
+  let url = process.env.MONGO_DSN
   res.json({
     data:url
   });
